8a1dd8fb4f75d5cc51aa45eed86be3abae1d0ee4
alex zook <zook.ae@gmail.com>
2014-02-03 09:30:06 -0500

    ability to load sprites from textures folder

---
+++ Assets/Scripts/Spawn/LoadSprites.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class LoadSprites : MonoBehaviour {
+
+#region singleton
+    // cf: http://clearcutgames.net/home/?p=437
+    // (v1) Allow manipulation in editor and prevent duplicates
+    // static singleton property
+    public static LoadSprites Singleton { get; private set; }
+
+    // instantiate on game start
+    void Awake() {
+
+        // check for conflicting instances
+        if (Singleton != null && Singleton != this) {
+            Destroy(gameObject); // destroy others that conflict
+        }
+
+        Singleton = this; // save singleton instance
+
+        DontDestroyOnLoad(gameObject); // ensure not destroyed b/t scenes
+
+        // cf: http://answers.unity3d.com/questions/576153/loading-a-sprite-unity-43-in-resource-folder-and-s.html
+        // load sprites up
+        Singleton.spriteDB = Resources.LoadAll<Sprite>(spritePath);
+        Singleton.spriteNames = new string[Singleton.spriteDB.Length];
+
+        for (int ii = 0; ii < Singleton.spriteNames.Length; ii++) {
+            Singleton.spriteNames[ii] = Singleton.spriteDB[ii].name;
+            Debug.Log("[LoadSprite]: loading " + Singleton.spriteNames[ii]);
+        }
+    }
+#endregion
+
+    /// <summary>
+    /// Store all sprites loaded from path
+    /// </summary>
+    private Sprite[] spriteDB;
+
+    /// <summary>
+    /// Names of sprites in the DB
+    /// </summary>
+    private string[] spriteNames;
+
+
+    /// <summary>
+    /// Path to folder containing sprites under "Resources" folder
+    /// </summary>
+    public string spritePath = "textures";
+
+    
+    /// <summary>
+    /// Returns the sprite with the given name
+    /// </summary>
+    /// <param name="spriteName"></param>
+    /// <returns></returns>
+    public Sprite getSprite(string spriteName) {
+        Sprite sprite = Singleton.spriteDB[Array.IndexOf(Singleton.spriteNames, spriteName)];
+        return sprite;
+    }
+    
+}
---
+++ Assets/Scripts/Spawn/SetSprite.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SetSprite : MonoBehaviour {
+
+    /// <summary>
+    /// Sprite to set object to
+    /// </summary>
+    public string targetSprite;
+
+    void Update() {
+        AssignSprite(targetSprite);
+    }
+
+    public void AssignSprite(string spriteName) {
+        SpriteRenderer mySprite = gameObject.GetComponent<SpriteRenderer>();
+        mySprite.sprite = LoadSprites.Singleton.getSprite(spriteName);
+    }
+	
+}
